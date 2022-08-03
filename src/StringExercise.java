cca33a477f2b6651ab9e8d6862c685f562a641ed
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-08-03 13:49:21 -0500

    Complete: Finished the string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static <function> void main(String[] args) {
+        String statement = "We don't need no education. We don't need no thought control";
+        String statement2 = "Check \"this\" out!, \"s inside of \"s!";
+        String statement3 = "In windows, the main drive is usually C:\\";
+        String statement4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(statement);
+        System.out.println(statement2);
+        System.out.println(statement3);
+        System.out.println(statement4);
+    }
+}
