57649d62d00ceb8eafec2578b4d9812b14e8f441
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-08-09 15:02:01 -0500

    Complete: Updated code in Person exercise

---
+++ src/java_two/ArraysExercises.java
@@ -0,0 +1,10 @@
+package java_two;
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
