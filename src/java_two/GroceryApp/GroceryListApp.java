2ae81b261bb110d85aee15c93a54f6d2de169889
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-08-16 14:43:18 -0500

    Complete: Finished Grocery list app

--- src/java_two/GroceryApp/GroceryListApp.java
+++ src/java_two/GroceryApp/GroceryListApp.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 
     public class GroceryListApp {
-        public static final int PRODUCE = 1;
-        public static final int MEATZ = 2;
+        public static final int Produce = 1;
+        public static final int Meat = 2;
+        public static final int Dairy = 3;
+        public static final int Seafood = 4;
+        public static final int Other = 5;
+
 
         private static Input input = new Input();
 
@@ -35,7 +39,7 @@ import java.util.Collections;
                 printGroceryCategories();
 
                 System.out.println("Enter the number of the category: ");
-                int categoryNum = input.getInt(1,2);
+                int categoryNum = input.getInt(1,5);
 
                 System.out.print("Enter the name of the item: ");
                 String itemName = input.getString();
@@ -57,14 +61,20 @@ import java.util.Collections;
         private static void printGroceryCategories() {
             System.out.println("Grocery Categories:");
             System.out.println("1. Produce");
-            System.out.println("2. Meatz");
+            System.out.println("2. Meat");
+            System.out.println("3. Dairy");
+            System.out.println("4. Seafood");
+            System.out.println("5. Other");
 
         }
 
         private static void printGroceryList(ArrayList<GroceryItem> groceries) {
             // 1. for each category
-            printGroceryListByCategory(PRODUCE, groceries);
-            printGroceryListByCategory(MEATZ, groceries);
+            printGroceryListByCategory(Produce, groceries);
+            printGroceryListByCategory(Meat, groceries);
+            printGroceryListByCategory(Dairy, groceries);
+            printGroceryListByCategory(Seafood, groceries);
+            printGroceryListByCategory(Other, groceries);
         }
 
         private static void printGroceryListByCategory(int category, ArrayList<GroceryItem> groceries) {
@@ -82,10 +92,16 @@ import java.util.Collections;
             Collections.sort(categoryItems);
 
             // 4. print them w quantity
-            if(category == PRODUCE) {
-                System.out.println("PRODUCE:");
+            if(category == Produce) {
+                System.out.println("Produce:");
+            } else if(category == Meat){
+                System.out.println("Meat:");
+            } else if(category == Dairy){
+                System.out.println("Dairy:");
+            } else if(category == Seafood){
+                System.out.println("Seafood:");
             } else {
-                System.out.println("MEATZ:");
+                System.out.println("Other:");
             }
             System.out.println(categoryItems);
         }
